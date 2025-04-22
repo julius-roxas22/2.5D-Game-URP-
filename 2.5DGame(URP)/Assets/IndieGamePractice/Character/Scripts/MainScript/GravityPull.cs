ac5af28fab8d474460a1f775d54b2a5ce07dcaaa
Julius Roxas <julius.roxas22@gmail.com>
2025-04-21 18:09:19 -0800

    #29 done

---
+++ 2.5DGame(URP)/Assets/IndieGamePractice/Character/Scripts/MainScript/GravityPull.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace IndieGamePractice
+{
+    [CreateAssetMenu(fileName = "New Ability Data", menuName = "IndieGamePractice/Create/Ability/GravityPull")]
+    public class GravityPull : StateData
+    {
+        [SerializeField] private AnimationCurve gravirtPull;
+        public override void _OnEnterAbility(CharacterStateBase characterStateBase, Animator animator, AnimatorStateInfo animatorStateInfo)
+        {
+
+        }
+
+        public override void _OnUpdateAbility(CharacterStateBase characterStateBase, Animator animator, AnimatorStateInfo animatorStateInfo)
+        {
+            CharacterControl control = characterStateBase._GetCharacterControl(animator);
+            control._GravityMultiplier = gravirtPull.Evaluate(animatorStateInfo.normalizedTime);
+        }
+
+        public override void _OnExitAbility(CharacterStateBase characterStateBase, Animator animator, AnimatorStateInfo animatorStateInfo)
+        {
+            CharacterControl control = characterStateBase._GetCharacterControl(animator);
+            control._GravityMultiplier = 0f;
+        }
+    }
+}
+
+
